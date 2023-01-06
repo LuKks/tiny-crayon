f5a47f5cd6ce03e1d18d7990b993e210aca2b5dc
Lucas Barrena <lucasbarrena98@gmail.com>
2023-01-06 06:50:48 -0300

    quick fix support for alt runtime

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = new Crayon()
 module.exports.Crayon = Crayon
 
-function Crayon (stream = process.stdout) {
+function Crayon (stream) {
+  if (!stream) stream = process._stdout ? { isTTY: true } : process.stdout
+
   const modifiers1 = { reset: [0, 0], bold: [1, 22], dim: [2, 22], italic: [3, 23], underline: [4, 24] }
   const modifiers2 = { overline: [53, 55], inverse: [7, 27], hidden: [8, 28], strikethrough: [9, 29] }
   const colors = { index: 30, list: ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'], close: 39 }
